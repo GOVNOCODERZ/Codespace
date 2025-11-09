c70b86c4bfc07844dd37bfb01217b5b6f4821392
PlayerUnknown14 <gordovvladislav@yandex.ru>
2025-11-09 20:11:59 +0700

    Фикс

--- C_codespace/2 kurs/3-rd senester/programming languages/laboratory work/5-th lab/Vlad/main.py
+++ C_codespace/2 kurs/3-rd senester/programming languages/laboratory work/5-th lab/Vlad/main.py
@@ -1,12 +1,12 @@
-from header import list_input, find_second_max_index, find_penultimate_min_index
+import header
 
 print("=== Ввод списка A ===")
-list_A = list_input()
+list_A = header.list_input()
 print("=== Ввод списка B ===")
-list_B = list_input()
+list_B = header.list_input()
 
-second_max = find_second_max_index(list_A)
-penultimate_min = find_penultimate_min_index(list_B)
+second_max = header.find_second_max_index(list_A)
+penultimate_min = header.find_penultimate_min_index(list_B)
 elem_sum = list_A[second_max] + list_B[penultimate_min]
 
 print(f"Индекс второго максимума списка A: {second_max}")
