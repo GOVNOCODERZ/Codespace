03a2c7bfe7075e0134268873a574c19b57f817e7
PlayerUnknown14 <gordovvladislav@yandex.ru>
2025-12-07 17:04:39 +0700

    Фиксы

--- C_codespace/2 kurs/3-rd senester/programming languages/laboratory work/9-th lab/Vlad/main.py
+++ C_codespace/2 kurs/3-rd senester/programming languages/laboratory work/9-th lab/Vlad/main.py
@@ -5,6 +5,15 @@ container = Shop()
 # Файл для записи/чтения
 filename = "E:/Codespace/C_codespace/2 kurs/3-rd senester/Programming languages/laboratory work/9-th lab/Vlad/storage_devices.txt"
 
+# Заполняем список начальными данными
+with open(filename, "w", encoding='utf-8') as f:
+    f.write("PortableHDD,WD Blue,1000,Western Digital,5000.0,5400\n")
+    f.write("DVD,Disk A,4.7,Generic,100.0,DVD-R\n")
+    f.write("PortableHDD,Seagate Fast,2000,Seagate,8000.0,7200\n")
+    f.write("DVD,Disk B,4.7,Verbatim,150.0,DVD+R\n")
+    f.write("PortableHDD,Toshiba Canvio,500,Toshiba,3500.0,5400\n")
+    f.write("DVD,Disk C,9.4,Verbatim,200.0,DVD-RW\n")
+    
 while True:
     print("\n--- База данных магазина накопителей ---")
     print("1. Загрузить накопители из файла")
