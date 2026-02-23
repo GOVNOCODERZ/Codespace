878f6e0d101daa734983868575424f6d80dea8a2
PlayerUnknown14 <gordovvladislav@yandex.ru>
2026-02-23 17:08:06 +0700

    4 задание (Python)

---
+++ Files/2-nd course/4-th semester/Information theory/Laboratory work/Lab #1/Vlad/main.py
@@ -0,0 +1,32 @@
+from header import TextEntropyAnalyzer
+
+def main():
+    analyzer = TextEntropyAnalyzer()
+    
+    while True:
+        print("\n--- МЕНЮ ---")
+        print("1. Загрузить файл")
+        print("2. Проанализировать файл")
+        print("3. Результаты анализа")
+        print("4. Таблица вероятностей")
+        print("0. Выход")
+        
+        choice = input("Ваш выбор: ")
+
+        if choice == "1":
+            filename = input("Введите название файла для анализа (Enter - 'input.txt'): ").strip() or "input.txt"
+            analyzer.load_file(filename)
+        elif choice == "2":
+            analyzer.analyze()
+        elif choice == "3":
+            analyzer.print_report()
+        elif choice == "4":
+            analyzer.print_probability_table(None)
+        elif choice == "0":
+            print("Выход из программы.")
+            break
+        else:
+            print("Неверный ввод.")
+
+if __name__ == "__main__":
+    main()
